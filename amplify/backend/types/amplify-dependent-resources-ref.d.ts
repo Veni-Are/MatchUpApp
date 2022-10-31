f3a81272e988786c3b18a0d9a5cd5d7d06530f9b
Mounika Devi Mamidibathula <mounikadevimamidibathula@Mounikas-MacBook-Pro.local>
2022-10-31 16:42:58 -0700

    added auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "matchupapp61e9175c": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
